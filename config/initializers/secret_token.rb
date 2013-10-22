71b304c3e30d1fcedfe771abe23cbf7785d5ccf5
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-10-22 00:24:54 -0500

    generation of rails new command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KlinkKloud::Application.config.secret_key_base = '0537a654cde3d6423dff0bfde16f51931123083260c13a25f10ebf0f5e9723df2cec906151a51db9e1a4c343f38be72dafb44fee08f96c49846517c8a41bf693'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KlinkKloud::Application.config.session_store :cookie_store, key: '_klink_kloud_session'
